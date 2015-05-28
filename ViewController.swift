f9cfa28e8b12f61ad7dd71c2976fcb577a2c47c7
darkrost <jspark@id4soft.com>
2015-05-28 17:27:28 +0900

    darkrost
    테스

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by 박종성 on 2015. 5. 28..
+//  Copyright (c) 2015년 Jspark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        NSLog("우하하하하하")
+        
+        NSLog("메롱")
+    }
+
+
+}
+
